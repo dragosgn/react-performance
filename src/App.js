270535dace3c4573be64b8b1a4731a5a7fbff2d8
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2018-04-10 12:31:11 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
-import logo from "./logo.svg";
 import "./App.css";
 import Color from "./Color";
 import colorList from "./colorList";
 
-const Root = styled.components`
+const Root = styled.div`
   padding: 2rem;
 `;
 
+const ColorBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {}
+
   render() {
     return (
       <Root>
         <p>Perfomance comparison in react</p>
-        <div className="container-fluid">
-          <ul>{}</ul>
-        </div>
+        <ColorBox>
+          {console.log(colorList)}
+          {colorList &&
+            colorList.map(color => {
+              const key = Math.random();
+              return (
+                <Color
+                  key={key}
+                  display={key.toFixed(2)}
+                  color={color}
+                  handleClick={this.handleClick}
+                />
+              );
+            })}
+        </ColorBox>
       </Root>
     );
   }
--- src/Color.js
+++ src/Color.js
@@ -4,10 +4,25 @@ import styled from "styled-components";
 const Root = styled.div`
   background-color: ${props => props.color};
   color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 5rem;
+  height: 5rem;
+  margin: 0.5rem;
+  cursor: pointer;
+  :hover {
+    background-color: red;
+  }
+`;
+
+const Id = styled.p`
+  font-size: 1.3rem;
+  font-weight: 400;
 `;
 
 export default ({ display, color, handleClick }) => (
   <Root onClick={handleClick} color={color}>
-    <p>{display}</p>
+    <Id>{display}</Id>
   </Root>
 );
